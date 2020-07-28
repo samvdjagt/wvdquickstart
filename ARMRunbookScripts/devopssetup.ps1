c74648406e379e54dc41e62d114dceafa5387d81
Samuel Van Der Jagt <t-savand@microsoft.com>
2020-07-28 12:50:57 -0600

    added detail to error messsage

--- ARMRunbookScripts/devopssetup.ps1
+++ ARMRunbookScripts/devopssetup.ps1
@@ -214,8 +214,8 @@ if ($identityApproach -eq 'AADDS') {
 
 $principalIds = (Get-AzureADGroup -SearchString $targetGroup).objectId
 if ($principalIds -eq $null) {
-  Write-Error "Did not find user group. Please check if the user group creation completed successfully."
-  throw "Did not find user group. Please check if the user group creation completed successfully."
+  Write-Error "Did not find user group $targetGroup. Please check if the user group creation completed successfully."
+  throw "Did not find user group $targetGroup. Please check if the user group creation completed successfully."
 }
 # In case the above search finds multiple groups, pick the first PrincipalId. Template only works when one principalId is supplied, not for multiple.
 $split = $principalIds.Split(' ')
