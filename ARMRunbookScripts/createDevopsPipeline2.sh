aad5f46f2b6189a8893d763c5315d18e029b0e13
Samuel Van Der Jagt <t-savand@microsoft.com>
2020-08-03 13:07:08 -0600

    update devops pipeline script

--- ARMRunbookScripts/createDevopsPipeline2.sh
+++ ARMRunbookScripts/createDevopsPipeline2.sh
@@ -5,12 +5,12 @@
 #     [Required]  ${1}  <orgName>        
 #     [Required]  ${2}  <projectName>         
 #     [Required]  ${3}  <azureAdminUpn>             
-#     [Required]  ${4}  <azureAdminPassword>
+#     [Required]  ${4}  <keyvaultName>
 #     [Required]  ${5}  <createPipeline>   
 
 
 az login --identity
-secret ="$(az keyvault secret show --name 'azurePassword' --vault-name ${4})" 
+$secret = az keyvault secret show --name 'azurePassword' --vault-name ${4} --query '[value]' -o tsv 
 az logout
 
 az login -u ${3} -p $secret
